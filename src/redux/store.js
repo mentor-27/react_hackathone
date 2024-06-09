941364235728f5ea1f1b13e8a3632fcb591c5713
Matadon27 <mexanoid2012@gmail.com>
2024-06-10 00:25:13 +0300

    redux fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk';
+import { createStore, combineReducers, compose } from 'redux';
 import { devsReducer } from './reducers';
 
 const reducer = combineReducers({
@@ -11,4 +10,4 @@ const composeEnhancers =
 		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) ||
 	compose;
 
-export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(reducer, composeEnhancers());
