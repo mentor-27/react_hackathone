ce747f876f1caf5080863434a710692235f5ca74
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-06-09 23:45:27 +0300

    fix

--- src/pages/DevPage/DevPage.jsx
+++ src/pages/DevPage/DevPage.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useMatch } from 'react-router-dom';
 
